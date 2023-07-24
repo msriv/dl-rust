9b4021e1d744640ecd94295b54cada8cfdc8c0d6
Mihir Srivastava <msrivastava574@gmail.com>
2023-07-24 14:11:44 +0530

    added new weights and biases to represent an output layer of 3 neurons

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,19 @@
 
 fn main() {
     let inputs: [f32; 4] = [1.0, 2.0, 3.0, 2.5];
-    let weights: [f32; 4] = [0.2, 0.8, -0.5, 1.0];
-    let bias: i8 = 2;
+    
+    let weights1: [f32; 4] = [0.2, 0.8, -0.5, 1.0];
+    let weights2: [f32; 4] = [0.5, -0.91, 0.26, -0.5];
+    let weights3: [f32; 4] = [-0.26, -0.27, 0.17, 0.87];
+    
+    let bias1: i8 = 2;
+    let bias2: i8 = 3;
+    let bias3: f32 = 0.5;
 
-    let output = inputs[0] as f32 * weights[0] + inputs[1] as f32 * weights[1] + inputs[2] as f32 * weights[2] + inputs[3] as f32 * weights[3] + bias as f32;
+    let output = [
+        inputs[0] as f32 * weights1[0] + inputs[1] as f32 * weights1[1] + inputs[2] as f32 * weights1[2] + inputs[3] as f32 * weights1[3] + bias1 as f32,
+        inputs[0] as f32 * weights2[0] + inputs[1] as f32 * weights2[1] + inputs[2] as f32 * weights2[2] + inputs[3] as f32 * weights2[3] + bias2 as f32,
+        inputs[0] as f32 * weights3[0] + inputs[1] as f32 * weights3[1] + inputs[2] as f32 * weights3[2] + inputs[3] as f32 * weights3[3] + bias3 as f32,
+    ];
     println!("{:?}", output)
 }
