01cfa7c09bc33c47660e093005ee1e1e7eac49c1
Mihir Srivastava <msrivastava574@gmail.com>
2023-07-31 09:44:26 +0530

    compute neuron value in a loop

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,24 @@
 fn main() {
     let inputs: [f32; 4] = [1.0, 2.0, 3.0, 2.5];
     
-    let weights1: [f32; 4] = [0.2, 0.8, -0.5, 1.0];
-    let weights2: [f32; 4] = [0.5, -0.91, 0.26, -0.5];
-    let weights3: [f32; 4] = [-0.26, -0.27, 0.17, 0.87];
-    
-    let bias1: i8 = 2;
-    let bias2: i8 = 3;
-    let bias3: f32 = 0.5;
-
-    let output = [
-        inputs[0] as f32 * weights1[0] + inputs[1] as f32 * weights1[1] + inputs[2] as f32 * weights1[2] + inputs[3] as f32 * weights1[3] + bias1 as f32,
-        inputs[0] as f32 * weights2[0] + inputs[1] as f32 * weights2[1] + inputs[2] as f32 * weights2[2] + inputs[3] as f32 * weights2[3] + bias2 as f32,
-        inputs[0] as f32 * weights3[0] + inputs[1] as f32 * weights3[1] + inputs[2] as f32 * weights3[2] + inputs[3] as f32 * weights3[3] + bias3 as f32,
+    let weights: [[f32; 4]; 3] = [
+        [0.2, 0.8, -0.5, 1.0], 
+        [0.5, -0.91, 0.26, -0.5], 
+        [-0.26, -0.27, 0.17, 0.87]
     ];
-    println!("{:?}", output)
+    
+    let biases: [f32; 3] = [2.0, 3.0, 0.5];
+
+    let mut layer_output: Vec<f32> = [].to_vec();
+
+    for i in 0..weights.len() { // Parent loop is looping on length of weights list as this will be equal to the length of output list
+        let mut neuron_val: f32 = 0.0;
+        for j in 0..inputs.len() { // Child loop is looping on the inputs list as this will be equal to the length of each weight list
+            neuron_val += weights[i][j] * inputs[j]
+        }
+
+        neuron_val += biases[i];
+        layer_output.push(neuron_val);
+    }
+    println!("{:?}", layer_output)
 }
